534c81e59d6a3374af31b85b8524d376c7a5f902
Rukmani <89690613+rukmani2556@users.noreply.github.com>
2022-01-28 20:27:45 +0530

    Create pattern8.cpp

---
+++ pattern8.cpp
@@ -0,0 +1,22 @@
+/*
+5
+1
+21
+321
+4321
+54321
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    int n; cin>>n;
+    for ( i = 1; i<=n; i++){
+        for ( j = i; j>=1; j--){    
+            cout<<j;    
+        }   
+        cout<<endl;
+    }
+    return 0;
+}
