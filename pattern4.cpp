3d410fbf88267a11af26feae8ef6d72a7cede025
Rukmani <89690613+rukmani2556@users.noreply.github.com>
2022-01-28 22:31:32 +0530

    Create pattern4.cpp

---
+++ pattern4.cpp
@@ -0,0 +1,22 @@
+/*
+5
+1
+12
+123
+1234
+12345
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    int n; cin>>n;
+    for ( i = 1; i<=n; i++){
+        for ( j = 1; j<=i; j++){    
+           cout<<j;
+        }                 
+        cout<<endl;
+    }
+    return 0;
+}
