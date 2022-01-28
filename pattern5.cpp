403a0ba15dab1b668f82e5965392f0f6b2a7def9
Rukmani <89690613+rukmani2556@users.noreply.github.com>
2022-01-28 19:47:49 +0530

    Create pattern5.cpp

---
+++ pattern5.cpp
@@ -0,0 +1,23 @@
+/*
+5
+1
+22
+333
+4444
+55555
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    int n; cin>>n;
+    for ( i = 1; i<=n; i++){
+        for ( j = 1; j<=i; j++)
+        {   
+            cout<<i;      
+        }
+        cout<<endl;
+    }
+    return 0;
+}
