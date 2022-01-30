c904ff096bcfb5c9c8087f7c519e123b6161e1b0
Rukmani <89690613+rukmani2556@users.noreply.github.com>
2022-01-30 12:15:11 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -11,7 +11,7 @@ int main(){
         cin>>arr[i];
     }
     int count = 1;
-    while (count < n){
+    while (count < n-1){
         for(int i = 0; i < n-count; i++){
             if(arr[i]>arr[i+1]){
                 int temp = arr[i];
