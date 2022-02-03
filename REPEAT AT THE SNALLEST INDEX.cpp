0531b08d6208f44c1c8cd8179e7161dd3e462adc
Rukmani <89690613+rukmani2556@users.noreply.github.com>
2022-02-03 14:36:14 +0530

    Update REPEAT AT THE SNALLEST INDEX.cpp

--- REPEAT AT THE SNALLEST INDEX.cpp
+++ REPEAT AT THE SNALLEST INDEX.cpp
@@ -5,7 +5,7 @@ TIME COMPLEXITY = O(n)
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;;cin>>n;
+    int n;cin>>n;
     int arr[n+1];
     arr[n] = -1;
     for (int i = 0; i < n; i++ ) {
@@ -17,11 +17,11 @@ int main(){
         idx[i] = -1;
     }
     int minidx = INT_MAX;
-    for(int i=0;i<n;i++){
-        if(idx[arr[i]]!=-1){
+    for(int i = 0; i < n; i++){
+        if(idx[arr[i]] != -1){
             minidx = min(minidx, idx[arr[i]]);
         }else{
-            idx[arr[i]]=i;
+            idx[arr[i]] = i;
         }    
     }  
     if(minidx == INT_MAX){
@@ -36,5 +36,4 @@ int main(){
 8 
 8 6 4 1 9 5 9 4 
 3
-
 */
